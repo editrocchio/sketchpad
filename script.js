38c5458d378c55be2fde6bdd9656fada1bb77ea1
Emilio <editrocchio@gmail.com>
2017-01-21 23:17:37 +0000

    changed some stuff

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 $(document).ready(function() {
   for (x = 0; x < 16; x++) {
-    $("<br>").appendTo("#container");
+    $("<br>").appendTo("#grid");
     for (y = 0; y < 16; y++) {
-      $('<div class="grid">').appendTo("#container");
+      $('<div class="boxes">').appendTo("#grid");
     };
   };
-  $(".grid").hover(function(){
+  $(".boxes").hover(function(){
     $(this).css("background-color","red");
   });
   $("#reset").click(function() {
-    $(".grid").css("background-color","transparent");
-    var size = prompt("Enter a size for the grid in pixels");
+    $(".boxes").css("background-color","transparent");
+    var size = prompt("Enter a size for the grid");
   });
   $("#schwifty").click(function() {
     $(location).attr('href','https://www.youtube.com/watch?v=n4Xp6g-_UUw')
   });
 });
+
+
+
